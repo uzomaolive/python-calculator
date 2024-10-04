0b857401d77938042032db843ea13b1060e3fdfb
Olive Uzoma <olive.c.uzoma@gmail.com>
2024-10-04 17:39:13 +0100

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,5 @@
+operator = input("Enter operator (+ - * /): ")
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
+
+print(num1 + num2)  # This will concatenate the two numbers instead of adding them
